8251c34e0bf1eb12f8e037bf39c9c8c00fca931c
unknown <smsinchanmaitra2007@gmail.com>
2021-11-06 13:37:46 +0530

    NEW

--- textanalyzerpy/settings.py
+++ textanalyzerpy/settings.py
@@ -94,10 +94,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_ADDRESS')
-DEFAULT_FROM_EMAIL = config('EMAIL_ADDRESS')
-SERVER_EMAIL = config('EMAIL_ADDRESS')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_HOST_USER = "owner.jzsculture.smaitra@gmail.com"
+DEFAULT_FROM_EMAIL = "owner.jzsculture.smaitra@gmail.com"
+SERVER_EMAIL = "owner.jzsculture.smaitra@gmail.com"
+EMAIL_HOST_PASSWORD = "vteycrywdcxsuavi"
 
 
 # Database
