0d9c58adb491798d440d9fff33d2f1069d155014
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-02-13 13:17:15 +0530

    Update settings.py

--- textanalyzerpy/settings.py
+++ textanalyzerpy/settings.py
@@ -37,7 +37,7 @@ MESSAGE_TAGS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-111fqv@s7c@h#sml70+63a4jw7sbpv&u_^+a%-0q4e5&vayn@="
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False or True
